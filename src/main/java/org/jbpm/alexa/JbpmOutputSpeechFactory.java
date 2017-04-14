6c6b90d7322dbff3daa20c023a14b936f95eb00f
Duncan Doyle <Duncan.Doyle@gmail.com>
2017-04-14 17:44:22 +0200

    Implemented OutputSpeech.

--- src/main/java/org/jbpm/alexa/JbpmOutputSpeechFactory.java
+++ src/main/java/org/jbpm/alexa/JbpmOutputSpeechFactory.java
@@ -7,47 +7,42 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.amazon.speech.ui.PlainTextOutputSpeech;
-import com.redhat.bpms.examples.mortgage.model.ShoppingCartItem;
 
 public class JbpmOutputSpeechFactory implements OutputSpeechFactory<PlainTextOutputSpeech> {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(JbpmOutputSpeechFactory.class);
-	
+
+	private final static int taskPageSize = 4;
+
 	private final List<TaskSummary> taskSummaries;
-	
+
 	private String speechText;
-	
+
 	public JbpmOutputSpeechFactory(List<TaskSummary> taskSummaries) {
 		this.taskSummaries = taskSummaries;
 	}
-	
+
 	@Override
 	public PlainTextOutputSpeech getOutputSpeech() {
 		PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
-		StringBuilder speechBuilder = new StringBuilder();
-		
-		/*
-		//Items
-		if (shoppingCart.shoppingCartItemList.isEmpty()) {
-			speechBuilder.append("no items.");
-		} else {
-			for (ShoppingCartItem nextItem: shoppingCart.shoppingCartItemList) {
-				speechBuilder.append(nextItem.quantity).append(" ").append(nextItem.product.getName());
-			}
-		}
-		speechBuilder.append(". ");
-		
-		//Total value
-		speechBuilder.append(TOTAL_ORDER_AMOUNT_TEXT).append(shoppingCart.cartTotal);
-		String speechText = speechBuilder.toString();
-		LOGGER.debug("SpeechText for ShoppingCart: " +  speechText);
-		*/
+		StringBuilder speechBuilder = new StringBuilder("You have " + taskSummaries.size() + " tasks in your inbox.");
+
+		speechBuilder.append("These are the first ")
+				.append((taskSummaries.size() <= taskPageSize) ? taskSummaries.size() : taskPageSize).append(" tasks.");
+
+		taskSummaries.stream().limit(taskPageSize).forEach(t -> {
+			speechBuilder.append("Task with id ").append(t.getId());
+			speechBuilder.append("has Name ").append(t.getName());
+			speechBuilder.append("has Process id ").append(t.getProcessId());
+			speechBuilder.append("has Priority ").append(t.getPriority());
+		});
+
 		String speechText = speechBuilder.toString();
 		this.speechText = speechText;
 		speech.setText(speechText);
 		return speech;
 	}
-	
+
 	public String getSpeechText() {
 		return speechText;
 	}
